769a9ec7bed7befb33ba456fad0a7a021483db27
marija-mikic <93914251+marija-mikic@users.noreply.github.com>
2023-06-19 18:53:21 +0200

    ading Interface for service

---
+++ Vehicle-Mono-BL/Interface/IMakeVehicle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using Vehicle_Mono_DAL.Interface;
+
+namespace Vehicle_Mono_BL.Interface
+{
+    public interface IMakeVehicle
+    {
+        //Task<PaginatedList<IMake>> GetAll(Paging paging, Sorting sorting, string searchTerm);
+
+
+        public Task<bool> Add(IMake make);
+        Task<IMake> GetMakeById(int id);
+
+        Task<bool> Edit(IMake make);
+        Task<List<IMake>> GetAll();
+        // Task<string?> GetAll(Sorting sorting, string searchTerm, Paging paging);
+        Task<bool> Remove(int id);
+    }
+}
+}
---
+++ Vehicle-Mono-BL/Interface/IModelVehicle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using Vehicle_Mono_DAL.Interface;
+
+namespace Vehicle_Mono_BL.Interface
+{
+    internal interface IModelVehicle
+    {
+
+        Task<bool> Add(IModelV model);
+        Task<bool> Delete(int id);
+        Task<PaginatedList<IModelV>> GetAllModels(Paging paging, Sorting sorting, Filtering filtering);
+        Task<IModelV> GetModelById(int id);
+        Task<bool> Edit(IModelV make);
+    }
+}
